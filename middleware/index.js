0f207566d7c9c84ccc37dde1639fc8edddba53dc
JMV2RE4_thdgit <jeffrey_m_vanhorn@homedepot.com>
2022-07-25 16:18:02 -0500

    added server contents to wrong file

--- middleware/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-const morgan = require('morgan');
-const cors = require('cors');
