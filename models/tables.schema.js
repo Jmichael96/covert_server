745c7459f0773a814fe8b66ea0ce4794f53f91f3
JMV2RE4_thdgit <jeffrey_m_vanhorn@homedepot.com>
2022-08-02 11:37:55 -0500

    created modes for seeds

--- models/tables.schema.js
+++ models/tables.schema.js
@@ -1,10 +1,23 @@
 module.exports = [
   {
     table: 'USERS',
-    schema: 'name:string, email:string, password:string, phone:string, date_created:date'
+    schema: [
+      { name: 'name', type: 'STRING', mode: 'REQUIRED' },
+      { name: 'email', type: 'STRING', mode: 'REQUIRED' },
+      { name: 'password', type: 'STRING', mode: 'REQUIRED' },
+      { name: 'phone', type: 'STRING', mode: 'REQUIRED' },
+      { name: 'date_created', type: 'DATE', mode: 'REQUIRED' }
+    ],
   },
   {
     table: 'REMINDERS',
-    schema: 'user_id:string, type:string, date_due:date, reminder_date:date, repeat:string, date_created:date'
+    schema: [
+      { name: 'user_id', type: 'STRING', mode: 'REQUIRED' },
+      { name: 'reminder_type', type: 'STRING', mode: 'REQUIRED' },
+      { name: 'date_due', type: 'DATE', mode: 'REQUIRED' },
+      { name: 'reminder_date', type: 'DATE', mode: 'REQUIRED' },
+      { name: 'repeat', type: 'STRING', mode: 'REQUIRED' },
+      { name: 'date_created', type: 'DATE', mode: 'REQUIRED' }
+    ]
   }
 ];
