41ff16c4f4753e1383556bac960dc0c24923ae4c
JMV2RE4_thdgit <jeffrey_m_vanhorn@homedepot.com>
2022-08-25 15:07:30 -0500

    fixed message undefined issue

--- reminderAPI/services/formatMessage.js
+++ reminderAPI/services/formatMessage.js
@@ -5,18 +5,18 @@ module.exports = formatMessage = (reminderType, msgObject) => {
   const curDate = new Date();
   const utcDate = mtz.tz(curDate, 'UTC');
 
-  const { userName, dateDue, reminderMessage, repeat, alertDaysPrior } = msgObject;
+  const { userName, reminder_message, repeat, alert_days_prior } = msgObject;
 
   if (reminderType === 'phone') {
     return `Hello, ${userName}! This is Covert_Server sending you a reminder message. 
-    \n ${reminderMessage} \n 
-    Date Due: ${alertDaysPrior <= 0 ? moment(utcDate).format('ll') : moment(utcDate).add(alertDaysPrior, 'days').format('ll')} \n
+    \n ${reminder_message} \n 
+    Date Due: ${alert_days_prior <= 0 ? moment(utcDate).format('ll') : moment(utcDate).add(alert_days_prior, 'days').format('ll')} \n
     ${repeat ? 'Note: This is a re-occuring reminder' : 'Note: This reminder is not re-occuring'}
     `;
   } else if (reminderType === 'email') {
     return `Hello, ${userName}! This is Covert_Server sending you a reminder message. <br />
-    ${reminderMessage} <br />
-    <b>Date Due:</b> ${alertDaysPrior <= 0 ? moment(utcDate).format('ll') : moment(utcDate).add(alertDaysPrior, 'days').format('ll')}  <br />
+    ${reminder_message} <br />
+    <b>Date Due:</b> ${alert_days_prior <= 0 ? moment(utcDate).format('ll') : moment(utcDate).add(alert_days_prior, 'days').format('ll')}  <br />
     ${repeat ? '<b>Note:</b> This is a re-occuring reminder' : '<b>Note:</b> This reminder is not re-occuring'}`
   }
 };
